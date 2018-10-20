d915925a7a73c7f998525c03f2a8b08053682a46
Stanley Huang <infamous_godhand@yahoo.com>
2018-10-20 23:41:19 +0000

    added margin/padding to svg container

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 /*global d3 */
-const svg = d3.select('svg');
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', 600)
+    .attr('height', 600);
+
+const margin = { top: 20, right: 20, bottom: 100, left: 100 };
+const graphWidth = 600 - margin.right - margin.left;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left},${margin.top})`);
 
 d3.json('menu.json').then(data => {
+
+
     const y = d3.scaleLinear()
-        .domain([0, 1000])
+        .domain([0, d3.max(data, d => d.orders)])
         .range([0, 500]);
 
     const x = d3.scaleBand()
@@ -12,7 +26,13 @@ d3.json('menu.json').then(data => {
         .paddingInner(0.2)
         .paddingOuter(0.2);
 
-    const rects = svg.selectAll('rect').data(data);
+    /*const min = d3.min(d => d.orders);
+    const max = d3.max(d => d.orders);
+    const extent = d3.extent(d => d.orders);
+    console.log(min, max);
+    */
+
+    const rects = graph.selectAll('rect').data(data);
 
     rects.attr('width', x.bandwidth)
         .attr('height', d => y(d.orders))
